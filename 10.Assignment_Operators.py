d0a8f5d68dfed9ff08ebd93a2a0f91640b24c8a5
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-10 22:11:58 +0000

    Add 10.Assignment_ Operators.py

---
+++ 10.Assignment_Operators.py
@@ -0,0 +1,11 @@
+a = 4-2     #assign the value of 4-2 to it
+
+print(a)
+
+b = 6
+
+b += 3      # increment the value of b by 3 and assign it to b
+print(b)
+
+b -=3       # decrement the value of b by 3 and assign it to b
+print(b)
