f410b9631fa9d3dc5e3173a127e80bc5d5aaed59
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-10 14:26:45 +0000

    Add Practice Set 1

---
+++ 03.Practice1.py
@@ -0,0 +1,10 @@
+# WAP to print a multi line poem
+
+# Either we can print each line one by one or got with the ''''''
+
+print('''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+''')
