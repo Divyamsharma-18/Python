6830845e5f1211be083277f00af7b0bd8139a50c
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-10 23:01:33 +0000

    Add Logical Operator

---
+++ 12.Logical_Operators.py
@@ -0,0 +1,22 @@
+a = True or False
+print(a)        
+
+
+# Truth Table of 'OR'
+
+print("True or False - ", True or False)
+print("True or True - ", True or True)
+print("False or True - ", False or True)
+print("False or False - ", False or False)
+
+# Truth Table of AND
+
+print("True and False - ", True and False)
+print("True and True - ", True and True)
+print("False and True - ", False and True)
+print("False and False - ", False and False)
+
+# 'NOT' Operators
+
+print(not(False))
+print(not(True))
