1e56bdb9706cadd6880bdb6ca083ad90b4b5ee8b
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-10 22:25:15 +0000

    Add 11.ComparisonO perators.py

--- 11.ComparisonOperators.py
+++ 11.ComparisonOperators.py
@@ -5,3 +5,27 @@ print(a)
 b = 2<3
 
 print(b)
+
+c = 5>=5
+
+print(c)
+
+d = 5<=5
+
+print(d)
+
+e = 5 == 5
+
+print(e)
+
+f = 5 == "5"
+
+print(f)
+
+g = 5 != 5
+
+print(g)
+
+h = 5 != 3
+
+print(h)
