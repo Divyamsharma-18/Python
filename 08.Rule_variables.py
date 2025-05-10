abf5035f723ca335267cbf988ca1a1a7bf51d2ea
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-10 21:25:46 +0000

    Add rules

---
+++ 08.Rule_variables.py
@@ -0,0 +1,21 @@
+#  Certain rules in naming the variable -
+# use alphabets, number, and unerscore
+# only starts with alphabets and undersore but never starts with number!
+
+a = 518
+
+dt = 29
+
+td = 4
+
+td518 = "Divit"
+
+dt_4 = "True"
+
+dt_29 = 1
+
+
+# Now what throws error - 
+@abcd = 2       # no special characters 
+
+1234 = 1234     # never starts with number
