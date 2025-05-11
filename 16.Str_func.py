9924f9219f8f173ac30c1474a53631ab946ae5a9
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-11 22:33:18 +0000

    Add String function

---
+++ 16.Str_func.py
@@ -0,0 +1,20 @@
+# Length
+
+name = "Divit"
+
+print(len(name))
+
+
+# Endswith
+
+print(name.endswith("it"))      # True
+print(name.endswith("ita"))     # False
+
+
+# Startswith
+
+print(name.startswith("Di"))        # True
+
+print(name.startswith("di"))        # False cause d is small
+
+print(name.startswith("abc"))        # False
