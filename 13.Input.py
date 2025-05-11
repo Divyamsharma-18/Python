3292d9fc4c577ebb25095ec7ee765e9935d23f72
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-11 20:54:01 +0000

    Add input data type

--- 13.Input.py
+++ 13.Input.py
@@ -1,5 +1,6 @@
-a = input("Enter number a : ")
-b = input("Enter number b : ")
+a = int(input("Enter number a : "))
+b = int(input("Enter number b : "))
 
 print("Number a is : ", a)
 print("Number b is : ", b)
+print("Sum is ", a + b)
