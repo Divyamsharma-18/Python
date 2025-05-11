5e0c919225f9d6c4a73ee31b6327be4c9b224035
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-11 22:43:34 +0000

    Add escape string func

---
+++ 17.Esc_Seq.py
@@ -0,0 +1,8 @@
+a = "He is a \\ good boy but\nnot \t a bad boy! \"Yeah\""      
+
+# \n adds new line 
+# \t add tab space
+#  \""\ add "" in b/w "" or use \''\ in ''
+# \\ for adding one \
+
+print(a)
