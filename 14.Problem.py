c08a986da48d74cb3dec15d2373fe42817d88dd2
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-05-11 21:19:34 +0000

    Add problem set

---
+++ 14.Problem.py
@@ -0,0 +1,40 @@
+# Add two numbers
+
+a = 5
+b = 18
+
+print("Sum = ", a+b)
+
+# Find remainder 
+
+c = 24
+
+d = 18
+
+print("Remainder of c and d : ", c%d)
+
+# Check type of variable
+
+print(type(a))
+
+# which is greater
+
+e = 33
+
+f = 80
+
+print("e is greater than f : ", e>f)
+
+# AVG of 2 numbers
+
+g = 10
+
+h = 20
+
+print("Avg of these two numbers are : ", (a+b)/2)       # things in bracket will be done first so don't depend on python doing BODMAS and stuff
+
+# square of number given by user
+
+i = int(input("Enter you number : "))
+
+print("The square of the number is : ", i**2)
